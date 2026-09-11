aa2845e11fc92e7b74636c1eff61b202a623b499
rafiyasana <rafiyasana0608@gmail.com>
2026-09-11 14:12:39 +0530

    Add files via upload

---
+++ day-9 tasks.py
@@ -0,0 +1,30 @@
+#student profile
+'''idno=int(input("enter idno:"))
+name=input("enter name:")
+mobileno=int(input("enter mobileno:"))
+mailid=input("enter mailid:")
+college=input("enter college:")
+branch=input("enter branch:")
+print(".............Student Profile.........")
+print("idno is",idno)
+print("name is",name)
+print("mobileno is",mobileno)
+print("mailid is",mailid)
+print("college is",college)
+print("branch is",branch)'''
+
+'''a=[10,20,30,40,50]
+a.insert(2,25)'''
+
+'''a=["python","java","c"]
+a.extend("code")'''
+
+'''a=("apple","banana","mango")
+b=list(a)
+b.append("kiwi")'''
+
+'''a={"black","white","red"}
+a.remove("white")'''
+
+'''a={"name":"sana","city":"vja"}
+a.update({"state":"ap"})'''
---
+++ runtime.py
@@ -0,0 +1,69 @@
+'''a=10
+b=20
+print(a+b)'''
+
+'''a=2
+b=5
+c=a+b
+print(c)'''
+
+'''print(4+9)'''
+
+#run_time input()
+'''a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+print(a+b)'''
+
+'''a=float(input("enter a value:"))
+b=float(input("enter b value:"))
+print(a+b)'''
+
+'''a=str(input("data1:"))
+b=str(input("data2:"))
+print(a+b)'''
+
+'''a=input("data1:")
+b=input("data2:")
+print(a+b)'''
+
+'''fname=input("first name:")
+lname=input("last name:")
+print((fname+" "+lname).title())'''
+
+'''a=complex(input("enter a value:"))
+b=complex(input("enter b value:"))
+print(a+b)'''
+
+'''a=bool(input("enter a value:"))
+b=bool(input("enter b value:"))
+c=bool(input("enter c value:"))
+print(a+b+c)'''
+
+'''a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+option=int(input("choose the option 1.add 2.sub 3.mul:"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+option=(input(choose the option
+                 1.add
+                 2.sub
+                 3.mul:))
+
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+option=int(input("choose the option add sub mul:"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
