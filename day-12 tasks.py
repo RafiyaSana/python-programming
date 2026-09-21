497709c25255da62af07ed5264a58cee705a9775
rafiyasana <rafiyasana0608@gmail.com>
2026-09-21 12:38:21 +0530

    Add files via upload

---
+++ day-12 tasks.py
@@ -0,0 +1,68 @@
+#voting
+'''age=int(input("enter the age:"))
+if age>=18:
+    print("eligible for vote")
+else:
+    print("not eligible for vote")'''
+
+#even or odd
+'''num=int(input("enter the number:"))
+if num%2==0:
+    print("numbers are even")
+else:
+    print("numbers are odd")'''
+
+#leap year
+'''year=int(input("enter a year:"))
+if year%4==0:
+    print("it is leap year")
+else:
+    print("it is not leap year")'''
+
+#vowels
+'''vowels=["a","e","i","o","u"]
+a=input("enter a letter:")
+if a in vowels:
+    print("it is vowel")
+else:
+    print("it is not vowel")'''
+
+#guest code
+'''name=input("enter a name:").lower()
+if name=="sana":
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+
+'''names=["sana","tasneem","abida","banu","rafiya"]
+a=input("enter a name:").lower()
+if a in names:
+    print("wlcome",a)
+else:
+    print("welcome guest")'''
+
+#bakery
+'''cake=int(input("enter price:"))
+if cake==1200:
+    print("Redvelvet cake")
+elif cake==1000:
+    print("choco almond cake")
+elif cake==800:
+    print("chocolate cake")
+elif cake==600:
+    print("butterscoth cake")
+else:
+    print("Sorry cake are not available")'''
+
+#pizza
+'''a=input("enter a pizza:")
+if a==("crispy chicken"):
+    print(800)
+elif a==("bbq"):
+    print(600)
+elif a==("paneer"):
+    print(400)
+elif a==("corn"):
+    print(300)
+elif a==("french fries and coke"):
+    print(200)'''
