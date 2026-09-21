b465e9f89f6d8ae7c275460d6f6b65a6a017b5a7
rafiyasana <rafiyasana0608@gmail.com>
2026-09-21 11:56:55 +0530

    Add files via upload

---
+++ real time tasks.py
@@ -0,0 +1,35 @@
+#nested-if
+'''username=input("enter the username:")
+password=int(input("enter the password:"))
+if username=="rafiya_sana":
+    if password==123456:
+        print("login successful")
+    else:
+        print("incorrect password")
+else:
+    print("invalid username")'''
+
+'''username=input("enter the username:")
+password=int(input("enter the password:"))
+if username=="rafiya_sana" and password==123456:
+        print("login successful")
+else:
+    print("invalid username")'''
+
+'''username=input("enter the username:")
+password=input("enter the password:")
+if username=="rafiya_sana" and password==123456:
+        print("login successful")
+else:
+    print("invalid credentials")'''
+
+#multiple-if
+'''age=int(input("enter age:"))
+attendence=int(input("enter attendence:"))
+marks=int(input("enter marks:"))
+if age>=18:
+    print("they are eligible for vote")
+if attendence>=70:
+    print("they are allow to write exams")
+if marks>=80:
+    print("they are ready for scholarship")'''
